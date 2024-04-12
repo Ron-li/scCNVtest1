016174bd1a5a1aade2a79c2add2067c2f2d52c36
Ron-li <70944526+Ron-li@users.noreply.github.com>
2024-04-12 11:40:09 -0400

    Initial commit

---
+++ R/supp_functions_1.4.R
@@ -0,0 +1,12 @@
+#' Create offtarget and ontarget reads
+#'
+#' This function uses the terminal to find overlap between fragments file and
+#' peaks. Bedtools and Tabix need to be installed in order for this
+#' preprocessing to occur
+#'
+#' @param x number
+#' @return the x^2
+#' @export
+testfunc <- function(x){
+  return(x^2)
+}
