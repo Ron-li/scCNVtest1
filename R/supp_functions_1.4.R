13a77f96df253b3416922864c72a22c65cfe23dc
Ron-li <70944526+Ron-li@users.noreply.github.com>
2024-04-12 11:48:27 -0400

    add one more function

--- R/supp_functions_1.4.R
+++ R/supp_functions_1.4.R
@@ -10,3 +10,18 @@
 testfunc <- function(x){
   return(x^2)
 }
+
+
+#' Convert Genomic Tiles to a dataframe
+#'
+#' This function converts a GRanges object to a dataframe
+#' with appropriate row names
+#'
+#' @param tiles GRanges Genomic Tiles Object
+#' @return A dataframe with genomic features as row names
+tileTodf <- function(tiles){
+  df <- as.data.frame(tiles)[,c(1:4)]
+  row_names <- do.call(paste, c(df[,c(1:3)], sep="-"))
+  rownames(df) <- row_names
+  return(df)
+}
